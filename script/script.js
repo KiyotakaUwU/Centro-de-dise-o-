94e8a3b2205e5e04606adc61a6db88bfa1e159f0
きよた <c4mo.sw@gmail.com>
2022-10-28 11:06:14 -0500

    Ultimo commit del archivo

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function showTime(){
+    myDate = new Date();
+    hours = myDate.getHours();
+    minutes = myDate.getMinutes();
+    seconds = myDate.getSeconds();
+    if (hours < 10) hours = 0 + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.querySelector(".hour").innerHTML = `${hours + ":" + minutes + ":" + seconds + ":"}`
+    setTimeout("showTime()", 1000);
+}
